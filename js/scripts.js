00b234af3a113b431f7ea095177d310734f796e5
Kevin Babcock and Malgorzata Haniszewska <kevin@>
2017-10-24 16:21:50 -0700

    ask user for a number and list all prime numbers less than user input

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+
+    var userNumber = parseInt($("#userNumber").val());
+    var numbers = [];
+    for (var index = userNumber; index >= 2; index --) {
+      numbers.push(index);
+    }
+
+    for (var prime = 2; prime < userNumber; prime ++) {
+      numbers.forEach(function(number) {
+        if (number !== prime) {
+          if (number % prime === 0) {
+            numbers.splice(numbers.indexOf(number), 1);
+          }
+        }
+      })
+    }
+    $(".hide").show();
+    $(".primeLessThan").text(userNumber);
+    $("#show").text(numbers);
+  })
+})
